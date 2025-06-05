ff98e880285fda0145c7ee40e1a8ef498c7d7868
Andy Mitchell <andy.mitchell.x+github@pm.me>
2025-06-05 05:51:35 -1000

    Will hopefully fix problems in itch.io

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, prelude::*};
 
 pub mod alien;
 pub mod alien_layouts;
@@ -16,6 +17,13 @@ fn main() {
         .add_plugins((
             //list of plugins added to the game
             DefaultPlugins
+                .set(AssetPlugin {
+                    // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                    // This causes errors and even panics on web build on itch.
+                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: String::from("Chain Reaxian"),
